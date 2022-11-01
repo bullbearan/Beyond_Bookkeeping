54e8e483353fb2f651851cdb005e43c74d294ff6
bullbearan <4403@holbertonstudents.com>
2022-11-01 10:52:11 +0100

    revomed sql file that creates the users table in the 'db' database

--- tables1.sql
+++
@@ -1,8 +0,0 @@
-USE db;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user_name` VARCHAR(255) NOT NULL,
-  `user_email` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
-); 
